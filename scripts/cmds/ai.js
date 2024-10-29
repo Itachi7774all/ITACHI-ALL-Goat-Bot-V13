3340d22849e7e45ac9d9161d547b6f5762ed44b9
Itachi7774all <fridolintshiyaz3101@gmail.com>
2024-10-29 21:39:49 +0200

    Update ai.js
    Signed-off-by: Itachi7774all <fridolintshiyaz3101@gmail.com>

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,1 +1,59 @@
+€cmd install Ai.js const axios = require('axios');
 
+async function fetchFromAI(url, params) {
+ try {
+ const response = await axios.get(url, { params });
+ return response.data;
+ } catch (error) {
+ console.error(error);
+ return null;
+ }
+}
+
+async function getAIResponse(input, userName, userId, messageID) {
+ const services = [
+ { url: 'https://ai-chat-gpt-4-lite.onrender.com/api/hercai', params: { question: input } }
+ ];
+
+ let response = ` salut, que puis-je faire pour toi ?`;
+ let currentIndex = 0;
+
+ for (let i = 0; i < services.length; i++) {
+ const service = services[currentIndex];
+ const data = await fetchFromAI(service.url, service.params);
+ if (data && (data.gpt4 || data.reply || data.response)) {
+ response = data.gpt4 || data.reply || data.response;
+ break;
+ }
+ currentIndex = (currentIndex + 1) % services.length; // Passer au service suivant
+ }
+
+ return { response, messageID };
+}
+module.exports = {
+  config: {
+    name: 'ai',
+    author: 'Arn',
+    role: 0,
+    category: 'ai',
+    shortDescription: 'ai to ask anything',
+  },
+  onStart: async function ({ api, event, args }) {
+    const input = args.join(' ').trim();
+    if (!input) {
+      api.sendMessage(`ITACHI II\n━━━━━━━━━━━━━━━━\nPlease provide a question or statement.\n━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
+      return;
+    }
+
+    const { response, messageID } = await getAIResponse(input, event.senderID, event.messageID);
+    api.sendMessage(`MESSIE OSANGO' \n━━━━━━━━━━━━━━━━\n${response}\n━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+  },
+  onChat: async function ({ event, message }) {
+    const messageContent = event.body.trim().toLowerCase();
+    if (messageContent.startsWith("ai")) {
+      const input = messageContent.replace(/^ai\s*/, "").trim();
+      const { response, messageID } = await getAIResponse(input, event.senderID, message.messageID);
+      message.reply(`☆𝔍𝔱𝔞𝔠𝔥𝔦 𝔲𝔠𝔥𝔦𝔴𝔞 𝔟𝔬𝔱☆\n__________________________________________\n${response}\n___________________________________`, messageID);
+    }
+  }
+};
